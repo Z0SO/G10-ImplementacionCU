eb897ee68a4df8fc92a5bca5fe83b7095930ef85
Z0SO <lautarorobales31@gmail.com>
2024-08-22 21:16:19 -0300

    agregado parte del front

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ DJANGO_APPS = [
 THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 
@@ -56,6 +57,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -183,4 +185,24 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 
 }
+
+
+
+# Configuracion de CORS
+# se va a utilizar svelte para el front
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5000",
+    "http://localhost:5000/",
+    "http://localhost:5000/*",
+]
+
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 
