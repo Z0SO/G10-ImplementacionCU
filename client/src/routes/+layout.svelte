ba48b06b51bb4980f1a836b0ce64666ac84abda8
Z0SO <lautarorobales31@gmail.com>
2024-08-24 03:00:50 -0300

    APP svelte funcional

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+
+<script>
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<main>
+  <slot></slot>
+</main>
+
+<style>
+  main {
+    padding: 1rem;
+  }
+</style>
---
+++ client/src/routes/turnos/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Turnos</h1>
+<p>Aquí puedes gestionar tus turnos.</p>
