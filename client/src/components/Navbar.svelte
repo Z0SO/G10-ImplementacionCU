01574933190b46a5d8c4943072caedb744659bf8
Z0SO <lautarorobales31@gmail.com>
2024-08-25 18:21:35 -0300

    modif: navbar

--- client/src/components/Navbar.svelte
+++ client/src/components/Navbar.svelte
@@ -34,7 +34,7 @@
         <li>
           <a
             href="/"
-            class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white hover:text-orange-500"
+            class="block py-2 pr-4 pl-3 text-gray-700 rounded md:bg-transparent md:p-0 dark:text-white hover:text-orange-500"
             aria-current="page"
           >
             Home
